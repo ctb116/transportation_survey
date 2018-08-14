df7342682f56ed7dc2175700239a66a926344c69
Catherine Bradley and Regina Nurieva <regina@>
2018-08-14 14:43:07 -0700

    add if else response based on feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
+
+    var userResponses = [];
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+      userResponses.push(workTransportationMode);
+      // $('#work-responses').append(workTransportationMode + "<br>");
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
-    });
+    for (var index = 0; index < userResponses.length; index+=1)
+
+      if (userResponses[index] === "bike" || userResponses[index] === "walk") {
+        $("#work-responses").show();
+      } else if (userResponses[index] === "skateboard" || userResponses[index] === "scooter" || userResponses[index] === "rollerblade") {
+      $("#dream-responses").show();
+    } else {
+        $("#fun-responses").show();
+    }
+
+
+    // if (userResponses === "bike") {
+    //   alert("You're good. You love to use muscle power");
+    // } else {
+    //   alert("You like to use gas");
+    // }
+
+    // $("#fun-responses").show();
+    // $("input:checkbox[name=fun-transportation]:checked").each(function(){
+    //   var funTransportationMode = $(this).val();
+    //   $('#fun-responses').append(funTransportationMode + "<br>");
+    // });
+    // $("#dream-responses").show();
+    // $("input:checkbox[name=dream-transportation]:checked").each(function(){
+    //   var dreamTransportationMode = $(this).val();
+    //   $('#dream-responses').append(dreamTransportationMode + "<br>");
+    // });
     $('#transportation_survey').hide();
   });
 });
